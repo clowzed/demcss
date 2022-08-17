ea9762cf2272dd9470394c6a0ddc6c452acfba7c
YMB DASHA <d2ms2nk@gmail.com>
2022-08-17 14:55:51 +0300

    Added dotenv and reading port env variable

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,31 @@ async fn index() -> impl actix_web::Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    info!("Trying to load environment variables from .env file");
+
+    match dotenv::dotenv() {
+        Ok(path) => info!(format!(
+            "Environment variables were loaded from {}",
+            path.into_os_string().into_string().unwrap()
+        )),
+        Err(e) => error!(format!(
+            "Failed to load environment variables from .env. Reason: {}",
+            e
+        )),
+    };
+
+    let port: u16 = match std::env::var("CSSING_PORT") {
+        Ok(port) => {
+            info!(format!("Port = {}", port));
+
+            match port.parse() {
+                Ok(p)  => p,
+                Err(_) => error!("Failed to parse port environment variable. Reason: Incorrect format (try using number)")
+            }
+        }
+        Err(_) => error!("Failed to read environment variable 'CSSING_PORT'"),
+    };
+
     let tera = tera::Tera::new("/templates").expect("Failed to initialize tera rendering engine");
 
     let app_data = actix_web::web::Data::new(std::sync::Mutex::new(AppData {
@@ -58,7 +83,7 @@ async fn main() -> std::io::Result<()> {
     }));
 
     actix_web::HttpServer::new(|| actix_web::App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", port))?
         .run()
         .await
 }
