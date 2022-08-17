6bb701845e96199e71aeee4e6608e1e3cf7e6064
YMB DASHA <d2ms2nk@gmail.com>
2022-08-17 12:39:18 +0300

    Test simple http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_web::get("/")]
+async fn index() -> impl actix_web::Responder {
+    actix_web::HttpResponse::Ok().body("hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    actix_web::HttpServer::new(|| actix_web::App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
