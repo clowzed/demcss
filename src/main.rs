9b4f5c3f6589e89df4a0a908c32a55fe8222bef0
YMB DASHA <d2ms2nk@gmail.com>
2022-08-17 14:44:37 +0300

    Tera rendering engine and app data were added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+
+struct AppData {
+    render_engine: tera::Tera,
+}
+
 #[actix_web::get("/")]
 async fn index() -> impl actix_web::Responder {
     actix_web::HttpResponse::Ok().body("hello")
@@ -5,6 +10,13 @@ async fn index() -> impl actix_web::Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    
+    let tera = tera::Tera::new("/templates").expect("Failed to initialize tera rendering engine");
+
+    let app_data = actix_web::web::Data::new(std::sync::Mutex::new(AppData {
+        render_engine: tera.clone(),
+    }));
+
     actix_web::HttpServer::new(|| actix_web::App::new().service(index))
         .bind(("127.0.0.1", 8080))?
         .run()
